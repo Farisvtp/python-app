10994cef584839b5281d5ab47ea1c3f01a1e1bc2
faris <hellofrs4037@gmail.com>
2026-07-07 16:48:44 +0530

    updated the code

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Kubernetes CI/CD with ArgoCD!..this is version 3"
+    return "Hello from Kubernetes CI/CD with ArgoCD!..this is version 4"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
